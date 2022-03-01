5722169b105057a27652fd6cb6bfe1f10e1debba
nathanoliveira3 <nathanoliveira3@gmail.com>
2022-03-01 10:33:31 -0300

    H2 database, test profile, JPA

--- src/main/java/com/project/store/resources/UserResource.java
+++ src/main/java/com/project/store/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserResource {
+
     @GetMapping
     public ResponseEntity<User> findAll() {
         User u = new User(1L, "Nathan", "nathanoliveira3@gmail.com", "123456", "12345");
