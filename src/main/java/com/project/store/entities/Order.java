32e73ef82b102e021152c290c1aeb4a3c5880c5d
nathanoliveira3 <nathanoliveira3@gmail.com>
2022-03-03 10:23:21 -0300

    OrderStatus enum

--- src/main/java/com/project/store/entities/Order.java
+++ src/main/java/com/project/store/entities/Order.java
@@ -2,6 +2,7 @@ package com.project.store.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.project.store.entities.en.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -19,20 +20,19 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
-    }
-
-    public User getClient() {
-        return client;
     }
 
     public Long getId() {
@@ -49,6 +49,20 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    public User getClient() {
+        return client;
     }
 
     public void setClient(User client) {
